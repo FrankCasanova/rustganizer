d466f48d02f738deade809116a076fa0c8481131
Frank Casanova <frankcasanova.info@gmail.com>
2025-05-02 12:06:14 +0200

    refactor(organizer): restructure file organization logic for maintainability
    The file organization logic in `mover.rs` has been refactored to improve maintainability. The parallel processing using Rayon has been replaced with a more straightforward sequential approach. This change simplifies the code and reduces complexity while maintaining the same functionality. Additionally, the `ui.rs` and `organizer.rs` files have been removed as their functionality has been consolidated into other modules.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crate::ui::views::run_ui;
+
+mod ui;
 mod organizer;
-mod ui; // Declare the ui module
+
 fn main() {
-    run_ui(); // Call the function to run the UI from the ui module
+    run_ui();
 }
