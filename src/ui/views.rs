10e0cfa4da631aeb37e491c218850cc411a326e2
Frank Casanova <frankcasanova.info@gmail.com>
2025-05-31 17:30:00 +0200

    new versión added with dropdown menu

--- src/ui/views.rs
+++ src/ui/views.rs
@@ -2,54 +2,84 @@
 
 use crate::organizer::mover::organize_files;
 use cursive::traits::*;
-use cursive::views::{Dialog, EditView, LinearLayout, TextView};
+use cursive::views::{Dialog, LinearLayout, TextView, SelectView};
+use std::process::Command;
+
+fn get_windows_users() -> Vec<String> {
+    let output = Command::new("cmd")
+        .args(["/C", "net user"])
+        .output();
+    if let Ok(output) = output {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let mut users = Vec::new();
+        let mut in_users = false;
+        for line in stdout.lines() {
+            if line.contains("---") {
+                in_users = !in_users;
+                continue;
+            }
+            if in_users {
+                users.extend(line.split_whitespace().map(|s| s.to_string()));
+            }
+        }
+        users
+    } else {
+        vec![]
+    }
+}
 
 pub fn run_ui() {
+    let users = get_windows_users();
     let mut siv = cursive::default();
+    let mut select = SelectView::<String>::new().with_all_str(users.clone());
+    select.add_item("<All Users>", "<ALL>".to_string());
+    select.set_on_submit(move |s, username: &String| {
+        let usernames: Vec<String> = if username == "<ALL>" {
+            users.clone()
+        } else {
+            vec![username.clone()]
+        };
+        let processing_dialog = Dialog::new()
+            .title("Organizing Files...")
+            .content(TextView::new("Please wait, organizing your files..."));
+        s.add_layer(processing_dialog);
+        let cb_sink = s.cb_sink().clone();
+        let usernames_clone = usernames.clone();
+        std::thread::spawn(move || {
+            let mut total_stats = crate::organizer::types::FileStats { music: 0, videos: 0, images: 0, docs: 0 };
+            let mut errors = Vec::new();
+            for user in usernames_clone {
+                match organize_files(&user, "en") {
+                    Ok(stats) => {
+                        total_stats.music += stats.music;
+                        total_stats.videos += stats.videos;
+                        total_stats.images += stats.images;
+                        total_stats.docs += stats.docs;
+                    }
+                    Err(e) => errors.push(format!("{}: {}", user, e)),
+                }
+            }
+            let info_message = format!(
+                "Organization Complete!\n\nMusic files/folders moved: {}\nVideo files/folders moved: {}\nImage files/folders moved: {}\nDocs files/folders moved: {}{}",
+                total_stats.music, total_stats.videos, total_stats.images, total_stats.docs,
+                if errors.is_empty() { "".to_string() } else { format!("\n\nErrors:\n{}", errors.join("\n")) }
+            );
+            cb_sink
+                .send(Box::new(move |s| {
+                    s.pop_layer();
+                    s.add_layer(Dialog::info(info_message));
+                }))
+                .unwrap();
+        });
+    });
     siv.add_layer(
         Dialog::new()
             .title("RustGanizer")
             .content(
                 LinearLayout::vertical()
-                    .child(TextView::new("Enter your Windows username:"))
-                    .child(EditView::new().with_name("username").fixed_width(50)),
+                    .child(TextView::new("Select the Windows user to organize:"))
+                    .child(select.with_name("user_select").fixed_width(50)),
             )
-            .button("Organize", |s| {
-                let username = s
-                    .call_on_name("username", |view: &mut EditView| view.get_content())
-                    .unwrap();
-                let processing_dialog = Dialog::new()
-                    .title("Organizing Files...")
-                    .content(TextView::new("Please wait, organizing your files..."));
-                s.add_layer(processing_dialog);
-                let cb_sink = s.cb_sink().clone();
-                std::thread::spawn(move || match organize_files(&username, "en") {
-                    Ok(stats) => {
-                        let info_message = format!(
-                            "Organization Complete!\n\n\
-                                 Music files/folders moved: {}\n\
-                                 Video files/folders moved: {}\n\
-                                 Image files/folders moved: {}\n\
-                                 Docs files/folders moved: {}",
-                            stats.music, stats.videos, stats.images, stats.docs
-                        );
-                        cb_sink
-                            .send(Box::new(move |s| {
-                                s.pop_layer();
-                                s.add_layer(Dialog::info(info_message));
-                            }))
-                            .unwrap();
-                    }
-                    Err(e) => {
-                        cb_sink
-                            .send(Box::new(move |s| {
-                                s.pop_layer();
-                                s.add_layer(Dialog::info(format!("Error organizing files: {}", e)));
-                            }))
-                            .unwrap();
-                    }
-                });
-            })
             .button("Close", |s| s.quit())
             .with_name("RustGanizer"),
     );
