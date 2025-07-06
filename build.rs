08d7d261bb29eddbf7e539d12d5ff7fadf6ada96
Frank Casanova <frankcasanova.info@gmail.com>
2025-07-06 17:51:24 +0200

    fix: remove unnecessary blank lines in build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
-
-
-fn main() {  
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {  
-        let mut res = winresource::WindowsResource::new();  
-        res.set_icon("assets/crap.ico");  
-        res.compile().unwrap();  
-    }  
+fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("assets/crap.ico");
+        res.compile().unwrap();
+    }
 }
